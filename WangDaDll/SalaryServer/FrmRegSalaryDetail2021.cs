e18c6e0834df051f584b15715ff7e5ae3c3c7880
zjm107 <35896875+zjm107@users.noreply.github.com>
2021-07-17 11:31:16 +0800

    Create FrmRegSalaryDetail2021.cs

---
+++ WangDaDll/SalaryServer/FrmRegSalaryDetail2021.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Tiger.Tools;
+
+namespace WangDaDll
+{
+    public partial class FrmRegSalaryDetail2021 : DevExpress.XtraEditors.XtraForm
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="salaryType">本年工资,上年工资</param>
+        public FrmRegSalaryDetail2021(int year, int month, string userID)
+        {
+            InitializeComponent();
+
+            this.Year = year;
+            this.Month = month;
+            this.UserID = userID;
+        }
+
+        /// <summary>
+        /// 年
+        /// </summary>
+        public int Year
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public string UserID
+        {
+            get; set;
+        }
+
+        public int Month
+        {
+            get; set;
+        }
+
+        private void FrmRegSalaryDetail2021_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                salaryDataSet.GetRegSumDetail(Year, Month, UserID);
+                salaryDataSetYW.GetBusinessSum(UserID, Year, Month);
+            }
+            catch (Exception ex)
+            {
+                UserMessages.ShowInfoBox(ex.Message);
+            }
+        }
+    }
+}
